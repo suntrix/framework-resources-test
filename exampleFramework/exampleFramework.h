e0523d933805a95e31200bd6d76a6426fdc6d559
Sebastian Owodziń <sebastian.owodzin@gmail.com>
2017-06-12 22:06:59 +0100

    Initial commit

---
+++ exampleFramework/exampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  exampleFramework.h
+//  exampleFramework
+//
+//  Created by Sebastian Owodzin on 11/06/2017.
+//  Copyright © 2017 Sebastian Owodzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for exampleFramework.
+FOUNDATION_EXPORT double exampleFrameworkVersionNumber;
+
+//! Project version string for exampleFramework.
+FOUNDATION_EXPORT const unsigned char exampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <exampleFramework/PublicHeader.h>
+
+
